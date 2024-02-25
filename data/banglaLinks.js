a7e1081f4472494ee238c4486a68f0a24e19ec60
piipiigii <86647086+piipiigii@users.noreply.github.com>
2024-02-25 20:48:48 +0530

    Bn page updated

--- data/banglaLinks.js
+++ data/banglaLinks.js
@@ -27,6 +27,20 @@ const banglaLinks = [
         url: "https://samayikprasanga.in/epaper.php",
         img: require('../public/assets/samp.webp')
     },
+    {
+        id: 3,
+        name: "একদিন",
+        about: "Ekdin",
+        url: "https://www.ekdin-epaper.com/",
+        img: require('../public/assets/ekdin.webp')
+    },
+    {
+        id: 4,
+        name: "পূবের কলম",
+        about: "Puber Kolom",
+        url: "https://epaper.puberkalom.com/",
+        img: require('../public/assets/pk.webp')
+    },
     
 ];
 
