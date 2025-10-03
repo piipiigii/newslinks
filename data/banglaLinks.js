f0b0c1e954693213e00063ddbda89b316404726d
piipiigii <86647086+piipiigii@users.noreply.github.com>
2025-10-03 20:06:53 +0530

    links updated

--- data/banglaLinks.js
+++ data/banglaLinks.js
@@ -6,43 +6,37 @@ const banglaLinks = [
         url: "https://www.eaajkaal.in/",
         img: require('../public/assets/aajk.webp')
     },
+  
     {
         id: 2,
-        name: "আনন্দবাজার পত্রিকা",
-        about: "Anandabazar Patrika",
-        url: "https://epaper.anandabazar.com/",
-        img: require('../public/assets/abp.webp')
-    },
-    {
-        id: 3,
 	name: "একদিন",
         about: "Ekdin",
         url: "https://www.ekdin-epaper.com/",
         img: require('../public/assets/ekdin.webp')        
     },
     {
-        id: 4,
+        id: 3,
         name: "পূবের কলম",
         about: "Puber Kolom",
         url: "https://epaper.puberkalom.com/",
         img: require('../public/assets/pk.webp')
     },
     {
-        id: 5,
+        id: 4,
         name: "সাময়িক প্ৰসঙ্গ",
         about: "Samayik Prasanga",
         url: "https://samayikprasanga.in/epaper.php",
         img: require('../public/assets/samp.webp')
     },
     {
-        id: 6,
+        id: 5,
         name: "সংবাদ প্রতিদিন",
         about: "Sangbad Pratidin",
         url: "https://epaper.sangbadpratidin.in/",
         img: require('../public/assets/sp.webp')
     },
     {
-        id: 7,
+        id: 6,
         name: "দৈনিক স্টেটসম্যান",
         about: "Dainik Statesman",
         url: "https://epaper.thestatesman.com/t/9256",
